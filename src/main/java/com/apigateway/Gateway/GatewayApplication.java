50373423c92a5874f7b7e931e06fad1223b3e9f6
Fernando Salamanca <fesaba21@gmail.com>
2024-03-17 00:43:49 -0500

    add eureka client conf

--- src/main/java/com/apigateway/Gateway/GatewayApplication.java
+++ src/main/java/com/apigateway/Gateway/GatewayApplication.java
@@ -1,11 +1,22 @@
 package com.apigateway.Gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
+	@Value("${server.port}")
+	int serverPort;
+
+	@RequestMapping("/")
+	public String home() {
+		return String.format("server-%s",serverPort);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
