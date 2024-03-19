a2eb5fab84b452fc3b1ff4a74a6d471a38fc5db1
Fernando Salamanca <fesaba21@gmail.com>
2024-03-19 10:18:36 -0500

    specify send email path

--- src/main/java/com/apigateway/Gateway/config/GatewayConfig.java
+++ src/main/java/com/apigateway/Gateway/config/GatewayConfig.java
@@ -65,6 +65,14 @@ public class GatewayConfig {
                         .uri("lb://MICROSERVICE_REPORTS")
                 )
                 .route(r -> r
+                        .path("/enviar-correo")
+                        .filters(f -> f
+                                .filter(jwtValidateTokenFilter.apply(config -> {config.setLogHeaders(false);}))
+                                .requestRateLimiter().configure(c -> c.setRateLimiter(redisRateLimiter()))
+                        )
+                        .uri("https://microserviciomaven-production.up.railway.app")
+                )
+                .route(r -> r
                         .path("/**")
                         .filters(f -> f
                                 .filter(jwtValidateTokenFilter.apply(config -> {config.setLogHeaders(false);}))
